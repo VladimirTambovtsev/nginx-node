122ad72aa3d437263f2c8c1d0ae53875265151ed
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-09-16 20:51:17 +0300

    sends file

--- sites/demo/index.js
+++ sites/demo/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get('/test', (req, res) => {
 	res.send('Node js response')
 })
 
